52d80dd2236f0443a378931a06647d606c4cac05
Bernardo <behornerl@gmail.com>
2019-11-17 13:56:47 -0300

    Script de controle do Chef (gatinho)

---
+++ ChefDasEsteira/Assets/Scripts/Chef.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chef : MonoBehaviour
+{
+    private GameObject currentIngredient;
+    [SerializeField] private Transform holdPosition;
+
+    public void HoldIngredient(GameObject ingredient)
+    {
+        if(currentIngredient == null)
+        {
+            currentIngredient = ingredient;
+            if (ingredient.GetComponent<Ingredient>().isInEsteira)
+            {
+                ingredient.GetComponent<Ingredient>().RemoveFromEsteira();
+            }
+            ingredient.transform.SetParent(holdPosition, false);
+            ingredient.transform.localPosition = Vector2.zero;
+        }
+    }
+
+    public void ReleaseIngredient()
+    {
+        currentIngredient = null;
+    }
+
+    public Ingredient.names GetIngredientName()
+    {
+        return currentIngredient.GetComponent<Ingredient>().ingredientName;
+    }
+
+    public bool IsHoldingIngredient()
+    { 
+        return currentIngredient != null;
+    }
+}
