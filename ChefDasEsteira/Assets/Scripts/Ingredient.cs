5d25027fd56c1015c2931ff28c60a91a3620d0ee
Bernardo <behornerl@gmail.com>
2019-11-17 13:50:17 -0300

    script ingrediente com comportamentos atualizados

--- ChefDasEsteira/Assets/Scripts/Ingredient.cs
+++ ChefDasEsteira/Assets/Scripts/Ingredient.cs
@@ -4,29 +4,23 @@ using UnityEngine;
 
 public class Ingredient : MonoBehaviour
 {
-    public enum ingredientNames { Salmon, Tuna, Rice, Algae }
-    public ingredientNames ingredientName;
-    [HideInInspector] public float moveSpeed;
-    private Vector3 directionalSpeed;
-    [SerializeField] private Rigidbody2D rb2d;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        directionalSpeed = new Vector3(moveSpeed, 0, 0);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        rb2d.MovePosition(transform.position + directionalSpeed);
-    }
+    public enum names { Salmao, Atum, Camarao, Arroz, Alga,
+                                SalmaoFatiado, AtumFatiado, CamaraoFatiado, ArrozCozido }
+    public names ingredientName;
+    public bool isInEsteira = true;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.tag == "fim esteira")
         {
+            RemoveFromEsteira();
             Destroy(this.gameObject);
         }
+    }
+
+    public void RemoveFromEsteira()
+    {
+        isInEsteira = false;
+        transform.parent.parent.GetComponent<EsteiraManager>().spawnedIngredients.Remove(gameObject);
     }
 }
