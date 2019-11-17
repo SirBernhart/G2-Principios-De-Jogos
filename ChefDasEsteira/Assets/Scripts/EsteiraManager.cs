fee0e1de2d25fc1392ad266e39e3c37a2a6c1fcc
Bernardo <behornerl@gmail.com>
2019-11-17 00:09:56 -0300

    Scipt de gerenciamento de ingredientes na esteira + tag de fim da esteira

---
+++ ChefDasEsteira/Assets/Scripts/EsteiraManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EsteiraManager : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> possibleIngredients;
+    private List<GameObject> spawnedIngredients = new List<GameObject>();
+    public float timeBetweenIngredients;
+    [SerializeField] private float moveSpeed;
+    private Vector3 directionalSpeed;
+    [SerializeField] private Transform spawnpoint;
+
+    private void Start()
+    {
+        timerNextIngredient = timeBetweenIngredients;
+    }
+
+    private float timerNextIngredient;
+    void Update()
+    {
+        timerNextIngredient += Time.deltaTime;
+        if(timerNextIngredient >= timeBetweenIngredients)
+        {
+            timerNextIngredient = 0;
+
+            GameObject nextIngredient = possibleIngredients[Random.Range(0, possibleIngredients.Count)];
+            Instantiate(nextIngredient, spawnpoint).GetComponent<Ingredient>().moveSpeed = moveSpeed;
+        }
+    }
+}
