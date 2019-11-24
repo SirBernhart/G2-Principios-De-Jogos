b96a04fde1366a1c1f6dede5026a7ebab7d9fc71
Bernardo <behornerl@gmail.com>
2019-11-24 11:08:55 -0300

    Novo script: ScoreManager

---
+++ ChefDasEsteira/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI scoreText;
+    private int currentScore;
+
+    private void Start()
+    {
+        scoreText.text = "0";
+    }
+
+    public void IncreaseScore(int scoreIncrement)
+    {
+        currentScore += scoreIncrement;
+        scoreText.text = currentScore.ToString();
+    }
+    public void DecreaseScore(int scoreDecrement)
+    {
+        currentScore -= scoreDecrement;
+        scoreText.text = currentScore.ToString();
+    }
+
+    public int GetScore()
+    {
+        return currentScore; 
+    }
+}
