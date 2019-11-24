cd85f7c8640ab04a28a6ff4fd462c2ce61535e81
Bernardo <behornerl@gmail.com>
2019-11-23 23:51:42 -0300

    Script de GameManager

---
+++ ChefDasEsteira/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private int maxErrorsAllowed;
+    private int errorCount;
+
+
+    public void IncreaseErrorCount(int errorIncrement)
+    {
+        errorCount += errorIncrement;
+        if(errorCount >= maxErrorsAllowed)
+        {
+            EndGame();
+        }
+    }
+
+    public void DecreaseErrorCount(int errorDecrement)
+    {
+        errorCount -= errorDecrement;
+        if (errorCount <= 0)
+            errorCount = 0;
+    }
+
+    public void EndGame()
+    {
+        // Load results screen
+        Debug.Log("Game over!");
+    }
+
+}
