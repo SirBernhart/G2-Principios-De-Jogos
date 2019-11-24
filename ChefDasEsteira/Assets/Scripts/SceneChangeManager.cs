5ea91d36da4e70839b2e26a8e2bd52ab9718c772
Bernardo <behornerl@gmail.com>
2019-11-24 18:33:51 -0300

    Controle de troca de cena

---
+++ ChefDasEsteira/Assets/Scripts/SceneChangeManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeManager : MonoBehaviour
+{
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
