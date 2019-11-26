7c0e6d9c2d57610de64cda809d5fb5677e79d6b9
Bernardo <behornerl@gmail.com>
2019-11-26 00:20:10 -0300

    Integração xunxada do Scriptable de Score com o ScoreManager

--- ChefDasEsteira/Assets/Scripts/ScoreManager.cs
+++ ChefDasEsteira/Assets/Scripts/ScoreManager.cs
@@ -6,21 +6,25 @@ using TMPro;
 public class ScoreManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private Score score;
     private int currentScore;
 
     private void Start()
     {
         scoreText.text = "0";
+        score.totalScore = 0;
     }
 
     public void IncreaseScore(int scoreIncrement)
     {
         currentScore += scoreIncrement;
+        score.totalScore = currentScore;
         scoreText.text = currentScore.ToString();
     }
     public void DecreaseScore(int scoreDecrement)
     {
         currentScore -= scoreDecrement;
+        score.totalScore = currentScore;
         scoreText.text = currentScore.ToString();
     }
 
