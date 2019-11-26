0f9a1ca1ae70b72cf98da88750e2fe4ff56558d7
Bernardo <behornerl@gmail.com>
2019-11-26 00:19:21 -0300

    Script esqueleto para o HighScore (agora só serve para mostrar a pontuação no final)

---
+++ ChefDasEsteira/Assets/Scripts/HighScore.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HighScore : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private Score score;
+
+    void Start()
+    {
+        scoreText.text = score.totalScore.ToString();
+    }
+}
