8248627f6dfa82671172d31ceee5fdf1b904d5ed
Bernardo <behornerl@gmail.com>
2019-11-26 00:18:12 -0300

    Novo scriptable object para guardar o Score entre as cenas

---
+++ ChefDasEsteira/Assets/Scripts/Scriptable Objects/Score.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Score", menuName = "New Score")]
+public class Score : ScriptableObject
+{
+    public int totalScore;
+}
