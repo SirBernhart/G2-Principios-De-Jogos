7de3955c89766ed6e107f4384676ddaebbeae325
Bernardo <behornerl@gmail.com>
2019-11-30 11:13:00 -0300

    Removendo código inutilizado

--- ChefDasEsteira/Assets/Scripts/Working Stations/WorkingStation.cs
+++ ChefDasEsteira/Assets/Scripts/Working Stations/WorkingStation.cs
@@ -8,50 +8,41 @@ public class WorkingStation : MonoBehaviour
     [SerializeField] private List<GameObject> acceptableIngredients;
     [SerializeField] private List<GameObject> possibleNewIngredients;
 
-    private void OnMouseEnter()
-    {
-        // Indica se o ingrediente selecionado é tratavel nessa estação ou não
-    }
-
     public bool TryPlaceIngredient(GameObject ingredient)
     {
         if (acceptableIngredients.Count == 0)
+        {
+            ingredient.transform.SetParent(transform, false);
+            ingredient.transform.localPosition = Vector3.zero;
             return true;
+        }
 
         for(int i = 0 ; i < acceptableIngredients.Count ; ++i)
         {
             if(acceptableIngredients[i].name+"(Clone)" == ingredient.name)
             {
                 ingredient.transform.SetParent(transform, false);
+                ingredient.transform.localPosition = Vector3.zero;
                 return true;
             }
         }
         return false;
     }
 
-    /*private void OnMouseUp()
+    public bool MakeNewIngredient()
     {
-        switch (chefRef.GetIngredientName())
-        {
-            case Ingredient.names.Salmao:
-
-                MakeNewIngredient(Ingredient.names.SalmaoFatiado);
-                break;
-        }
-    }*/
-
-    public void MakeNewIngredient()
-    {
+        Debug.Log(possibleNewIngredients.Count);
         for(int i = 0 ; i < possibleNewIngredients.Count ; ++i)
         {
-            if(possibleNewIngredients[i].GetComponent<Ingredient>().requiredIngredients[0].name + "(Clone)" == transform.GetChild(1).name)
+            if(possibleNewIngredients[i].GetComponent<Ingredient>().requiredIngredients[0].name + "(Clone)" == transform.GetChild(0).name)
             {
                 GameObject newIngredient = Instantiate(possibleNewIngredients[i], transform);
-                newIngredient.GetComponent<Ingredient>().isInEsteira = false;
                 newIngredient.transform.localPosition = Vector2.zero;
-                Destroy(transform.GetChild(1).gameObject);
+                Destroy(transform.GetChild(0).gameObject);
+                return true;
             }
         }
+        return false;
     }
 
 }
