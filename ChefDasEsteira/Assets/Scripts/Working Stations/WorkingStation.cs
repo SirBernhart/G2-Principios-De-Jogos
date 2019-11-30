fcd04b344d5febddec9e5a20d6ee262e14b83b48
Bernardo <behornerl@gmail.com>
2019-11-30 16:03:18 -0300

    Nova lógica necessária para ingredientes compostos por mais de um ingrediente

--- ChefDasEsteira/Assets/Scripts/Working Stations/WorkingStation.cs
+++ ChefDasEsteira/Assets/Scripts/Working Stations/WorkingStation.cs
@@ -31,18 +31,60 @@ public class WorkingStation : MonoBehaviour
 
     public bool MakeNewIngredient()
     {
-        Debug.Log(possibleNewIngredients.Count);
-        for(int i = 0 ; i < possibleNewIngredients.Count ; ++i)
+        Ingredient[] ingredientsInStation = LoadIngredientsInStation();
+
+        // Ingredients that can be made in this working station
+        for (int i = 0 ; i < possibleNewIngredients.Count; ++i)
         {
-            if(possibleNewIngredients[i].GetComponent<Ingredient>().requiredIngredients[0].name + "(Clone)" == transform.GetChild(0).name)
+            List<GameObject> requiredIngredients = possibleNewIngredients[i].GetComponent<Ingredient>().requiredIngredients;
+            if (requiredIngredients.Count != ingredientsInStation.Length)
             {
+                continue;
+            }
+
+            int j = 0;
+            // Ingredients that are required to make the possible new ingredients
+            for (; j < requiredIngredients.Count; ++j)
+            {
+                int k = 0;
+                // Ingredients that have been placed on the working station
+                for (; k < ingredientsInStation.Length; ++k)
+                {
+                    if ((requiredIngredients[j].name + "(Clone)") == ingredientsInStation[k].name)
+                    {
+                        break;
+                    }
+                }
+                // This new ingredient can't be made with the ingredients in the station
+                if(ingredientsInStation.Length == k)
+                {
+                    break;
+                }
+            }
+            if(j == requiredIngredients.Count)
+            {
+                ClearIngredientsInStation();
                 GameObject newIngredient = Instantiate(possibleNewIngredients[i], transform);
                 newIngredient.transform.localPosition = new Vector3(0, 0, -1);
-                Destroy(transform.GetChild(0).gameObject);
+                
                 return true;
             }
         }
+        
         return false;
+    }
+
+    private Ingredient[] LoadIngredientsInStation()
+    {
+        return transform.GetComponentsInChildren<Ingredient>();
+    }
+
+    private void ClearIngredientsInStation()
+    {
+        for(int i = 0 ; i < transform.childCount ; ++i)
+        {
+            Destroy(transform.GetChild(i).gameObject);
+        }
     }
 
 }
