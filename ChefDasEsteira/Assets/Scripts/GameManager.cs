c14b218f0d4dcbf91baf85dd30d425459b384f8c
Bernardo <behornerl@gmail.com>
2019-12-01 01:09:47 -0300

    Ajustes da música e fade nas cenas de game e game over

--- ChefDasEsteira/Assets/Scripts/GameManager.cs
+++ ChefDasEsteira/Assets/Scripts/GameManager.cs
@@ -9,6 +9,7 @@ public class GameManager : MonoBehaviour
     private int errorCount;
     private bool isFinishing = false;
     [SerializeField] private GameObject endGameFadeOut;
+    [SerializeField] private AudioSource music;
 
 
     public void IncreaseErrorCount(int errorIncrement)
@@ -30,7 +31,18 @@ public class GameManager : MonoBehaviour
     public void EndGame()
     {
         endGameFadeOut.SetActive(true);
-        Invoke("LoadGameOver", 3f);
+        StartCoroutine(FadeMusic());
+        Invoke("LoadGameOver", 3.5f);
+    }
+
+    IEnumerator FadeMusic()
+    {
+        for(float i = 3 ; i >= 0 ; i -= Time.deltaTime)
+        {
+            music.volume = i/3;
+            Debug.Log(music.volume);
+            yield return null;
+        }
     }
 
     public void LoadGameOver()
