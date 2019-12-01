bfbeb791363a09e502d14df84937a92b96d3cc25
Bernardo <behornerl@gmail.com>
2019-12-01 13:12:56 -0300

    Controle para fazer a música loopar corretamente

---
+++ ChefDasEsteira/Assets/Scripts/MusicController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    [SerializeField] AudioSource musicLoop;
+    [SerializeField] AudioSource musicIntro;
+
+    private void Start()
+    {
+        StartCoroutine(WaitToStartLoop());
+    }
+
+    IEnumerator WaitToStartLoop()
+    {
+        while (true)
+        {
+            if (!musicIntro.isPlaying)
+            {
+                musicLoop.Play();
+                break;
+            }
+            yield return null;
+        }
+    }
+}
